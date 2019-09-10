56f3deb36bf313cf130173b6fb928525261f10a9
elwazana <elwazana@mcmaster.ca>
2019-09-10 12:58:39 -0400

    Implemented Point2D constructors and related functions

--- MathLib/mathLib2D.cpp
+++ MathLib/mathLib2D.cpp
@@ -4,3 +4,30 @@
 // Implement functions here.
 
 // Don't write a main function, do that in main.cpp
+
+// Constructors for Point2D
+Point2D::Point2D() {
+  mX = 0.0;
+  mY = 0.0;
+}
+
+Point2D::Point2D(float inX, float inY) {
+  mX = inX;
+  mY = inY;
+}
+
+// Methods of a Point2D
+float Point2D::distanceTo(Point2D other) {
+  float left  = pow((other.mX - mX), 2);
+  float right = pow((other.mY - mY), 2);
+  float d     = sqrt(left + right);
+  return d;
+}
+
+float Point2D::fastDistanceTo(Point2D other) {
+  float left  = pow((other.mX - mX), 2);
+  float right = pow((other.mY - mY), 2);
+  float d     = left + right;
+  return d;
+}
+
